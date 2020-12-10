bd05196d05345e8ba4e4772c715729e00b2815b2
Jo2377 <johana.labou@dauphine.eu>
2020-12-10 20:09:25 +0100

    BHV: methodes recruterPersonnel, licencierPersonnel et deplacerLot dans la classe Entrepot (a revoir)

--- src/ChefBrico.java
+++ src/ChefBrico.java
@@ -3,7 +3,4 @@ public class ChefBrico extends ChefEquipe {
     public ChefBrico(String nom, String prenom) {
         super(nom, prenom);
     }
-
-    // La classe Meuble est a creer:
-    // public Meuble monterMeuble(){}
 }
